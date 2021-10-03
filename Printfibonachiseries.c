df07b08b2cd4bb76afecce0967317b7891caa7ef
Yogesh Jangid <49384150+yogesh584@users.noreply.github.com>
2021-10-03 21:27:19 +0530

    Update Printfibonachiseries.c

--- Printfibonachiseries.c
+++ Printfibonachiseries.c
@@ -8,3 +8,41 @@ Output: 0 1 1 2 3 5 8 13 21 34
 
 
 */
+// Fibonacci Series in C without recursion
+#include<stdio.h>    
+int main()    
+{    
+ int n1=0,n2=1,n3,i,number;    
+ printf("Enter the number of elements:");    
+ scanf("%d",&number);    
+ printf("\n%d %d",n1,n2);//printing 0 and 1    
+ for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+ }
+// Fibonacci Series in C with recursion
+#include<stdio.h>    
+void printFibonacci(int n){    
+    static int n1=0,n2=1,n3;    
+    if(n>0){    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+         printf("%d ",n3);    
+         printFibonacci(n-1);    
+    }    
+}    
+int main(){    
+    int n;    
+    printf("Enter the number of elements: ");    
+    scanf("%d",&n);    
+    printf("Fibonacci Series: ");    
+    printf("%d %d ",0,1);    
+    printFibonacci(n-2);//n-2 because 2 numbers are already printed    
+  return 0;  
+ }    
